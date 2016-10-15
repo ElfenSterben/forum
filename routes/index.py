96f0641440b9966eb0f2aea3e654aadd19902be5
ElfenSterben <362051950@qq.com>
2016-10-15 20:17:56 +0800

    没有数据也能正常显示(beta)

--- routes/index.py
+++ routes/index.py
@@ -7,19 +7,34 @@ main = Blueprint('index', __name__)
 
 @main.route('/')
 def index():
-    node_name = request.args.get('node')
-    selected_node = Node.query.get(1)
-    node = Node.query.filter_by(name=node_name).first()
-    if node is not None:
-        selected_node = node
-    post_list = selected_node.posts
-    post_list.reverse()
+    post_list = Post.query.order_by(Post.created_time.desc()).all()
     u = current_user()
     node_list = Node.query.all()
     data = {
         'post_list': post_list,
         'user': u,
         'node_list': node_list,
+        'select_all': True,
+        'selected_node': None
+    }
+    data_list.update(data)
+    return render_template('index.html', **data_list)
+
+@main.route('/<string:node_name>')
+def node_index(node_name):
+    selected_node = Node.query.filter_by(name=node_name).first()
+    if selected_node is None:
+        return redirect('index.index')
+    post_list = selected_node.posts
+    if len(post_list) != 0:
+        post_list.reverse()
+    u = current_user()
+    node_list = Node.query.all()
+    data = {
+        'post_list': post_list,
+        'user': u,
+        'node_list': node_list,
+        'select_all': False,
         'selected_node': selected_node
     }
     data_list.update(data)
