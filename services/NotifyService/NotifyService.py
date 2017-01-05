1869b201e95b1e5293efcdbfa3881ddf17215949
elfensterben <362051950@qq.com>
2017-01-05 19:12:59 +0800

    公告拉取写完

--- services/NotifyService/NotifyService.py
+++ services/NotifyService/NotifyService.py
@@ -38,4 +38,19 @@ class NotifyService(object):
         UserNotify.new(data)
 
     def pull_announce(self, user):
-        un = user.user_notifies.notify
+        last_announce = user.user_notifies.notify.filter_by(type=NOTIFY_TYPE.ANNOUNCE).last()
+        if last_announce is not None:
+            last_time = last_announce.created_time
+            announce = Notify.query.filter_by(type=NOTIFY_TYPE.ANNOUNCE)
+            ns = announce.filter('created_time>:created_time').params(created_time=last_time).all()
+            for n in ns:
+                data = {
+                    'user_id': user.id,
+                    'notify_id': n.id
+                }
+                UserNotify.new(data)
+
+    def pull_remind(self, user):
+        subscriptions = user.subscriptions
+        for s in subscriptions:
+            n = Notify.query.filter_by(target_id=s.target_id, target_type=s.target_type, action=s.action).filter('created_time>:created_time').params(created_time=s.created_time)
